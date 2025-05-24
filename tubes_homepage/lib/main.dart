2b9aaf2d70e3d591d4a08bee48869978ef33048b
Pkylar <purindakylalarissa@gmail.com>
2025-05-24 22:43:12 +0700

    Update main.dart

--- tubes_homepage/lib/main.dart
+++ tubes_homepage/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Final Project',
+      title: 'Final Project ABP',
       theme: ThemeData(
         useMaterial3: true,
       ),

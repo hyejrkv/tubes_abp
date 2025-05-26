3ff4829547d427508e997ed7783c2d2215617a9b
Nurul Izzah <147795093+hyejrkv@users.noreply.github.com>
2025-05-27 00:59:30 +0700

    ganti landing ke splash screen

--- tubes_homepage/lib/main.dart
+++ tubes_homepage/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'pages/home_page.dart';
+import 'pages/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
       ),
-      home: const HomePage(), // GANTI ke HomePage
+      home: const SplashScreen(), // ganti ke splash screen
     );
   }
 }
